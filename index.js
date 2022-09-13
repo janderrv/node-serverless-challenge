30ab1541fc1ad2d01ca355187e8912544a40b6d3
janderrv26@gmail.com <janderrv26@gmail.com>
2022-09-13 18:14:54 -0400

    feat: add serverless offline

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const app = express()
 const AWS = require('aws-sdk')
 
 const EMPLOYEES_TABLE = process.env.EMPLOYEES_TABLE
-const dynamoDb = new AWS.DynamoDB.DocumentClient()
+
+const IS_OFFLINE = process.env.IS_OFFLINE
+let dynamoDb
+if (IS_OFFLINE === 'true') {
+  dynamoDb = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000'
+  })
+  console.log(dynamoDb)
+} else {
+  dynamoDb = new AWS.DynamoDB.DocumentClient()
+}
 
 app.use(express.json())
 
